e95852a402cd1c3107c5bc4004bfb6c27473349c
vijay <ksvijay711@gmail.com>
2026-01-07 23:05:26 +0530

    add tailwind and postcss config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                dark: {
+                    900: '#0a0a0a',
+                    800: '#171717',
+                    700: '#262626',
+                }
+            }
+        },
+    },
+    plugins: [],
+}
